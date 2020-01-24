0a3e39263f6009d6b70476726d95d4f87e1f4f47
Oleg Ferenc <olegferenc1@gmail.com>
2020-01-24 20:04:00 +0200

    Add TypeORM

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,19 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
+import {RecipeModule} from './recipe/recipe.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+    imports: [RecipeModule, TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        entities: [],
+        synchronize: true,
+    }), ConfigModule.forRoot()]
 })
-export class AppModule {}
+export class AppModule {
+}
