b772856d816103f4c7195ab546dff769177bf0ff
Oleg Ferenc <olegferenc1@gmail.com>
2020-01-24 21:07:38 +0200

    Fix TypeORM configuration

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,16 +4,18 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {ConfigModule} from "@nestjs/config";
 
 @Module({
-    imports: [RecipeModule, TypeOrmModule.forRoot({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        entities: [],
-        synchronize: true,
-    }), ConfigModule.forRoot()]
+    imports: [RecipeModule,
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,
+        })]
 })
 export class AppModule {
 }
