1403d419c98bee0ab9693da128658f3f1bb98d67
Oleg Ferenc <olegferenc1@gmail.com>
2020-01-25 10:49:13 +0200

    Fix tslint errors

--- api/src/recipe/recipe.module.ts
+++ api/src/recipe/recipe.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import {TypeOrmModule} from '@nestjs/typeorm';
 import {Recipe} from './recipe.entity';
-import {RecipeService} from "./recipe.service";
-import {RecipeController} from "./recipe.controller";
+import {RecipeService} from './recipe.service';
+import {RecipeController} from './recipe.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Recipe])],
     providers: [RecipeService],
-    controllers: [RecipeController]
+    controllers: [RecipeController],
 })
 export class RecipeModule {}
--- api/src/recipe/recipe.service.ts
+++ api/src/recipe/recipe.service.ts
@@ -1,7 +1,7 @@
-import {Injectable} from "@nestjs/common";
-import {InjectRepository} from "@nestjs/typeorm";
-import {Recipe} from "./recipe.entity";
-import {Repository} from "typeorm";
+import {Injectable} from '@nestjs/common';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Recipe} from './recipe.entity';
+import {Repository} from 'typeorm';
 
 @Injectable()
 export class RecipeService {
