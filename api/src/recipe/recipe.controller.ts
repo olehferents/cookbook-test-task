d95f2b0835ffdcb0cc286ac3f9f6abaf9becaf8c
Oleg Ferenc <olegferenc1@gmail.com>
2020-01-25 14:35:25 +0200

    Add POST request handler

--- api/src/recipe/recipe.controller.ts
+++ api/src/recipe/recipe.controller.ts
@@ -1,12 +1,18 @@
-import {Controller, Get} from "@nestjs/common";
-import {RecipeService} from "./recipe.service";
+import {Body, Controller, Get, Post, Req, ValidationPipe} from '@nestjs/common';
+import {RecipeService} from './recipe.service';
+import {CreateRecipeDto} from './dto/create-recipe.dto';
 
-@Controller()
+@Controller('recipes')
 export class RecipeController {
     constructor(private readonly recipeService: RecipeService) {}
 
-    @Get('recipes')
+    @Get()
     async findAll() {
         return await this.recipeService.findAll();
+    }
+
+    @Post()
+    async create(@Body() createRecipeDto: CreateRecipeDto) {
+        return await this.recipeService.create(createRecipeDto);
     }
 }
