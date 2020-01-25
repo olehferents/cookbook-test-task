f194ab71a606b40d875ed560ac12e40cf08b17ec
Oleg Ferenc <olegferenc1@gmail.com>
2020-01-25 14:26:37 +0200

    Add ValidationPipe

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ValidationPipe} from '@nestjs/common';
+import {json, urlencoded} from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(urlencoded({ extended: true }));
+  app.use(json());
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
