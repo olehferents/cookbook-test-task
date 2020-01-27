00847088ec20ce284d12d6a042c6e32de23a56a6
Oleg Ferenc <olegferenc1@gmail.com>
2020-01-27 22:00:31 +0200

    Add edit form

--- frontend/src/components/Recipe/Recipe.js
+++ frontend/src/components/Recipe/Recipe.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Recipe.module.css';
 
 const Recipe = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [recipe, setRecipe] = useState({...props});
+
+    const toggleEdit = () => {
+        setIsEditing(!isEditing);
+    };
+
+    const handleValueChange = (e) => {
+        setRecipe({...recipe, [e.target.name]: e.target.value});
+    };
+
+    const handleSubmit = () => {
+        props.editRecipe(
+            recipe.id,
+            recipe.title,
+            recipe.description,
+            recipe.author
+        ).then(() => {
+            props.fetchRecipe();
+        });
+    };
+
+    if (isEditing) {
+        return (
+            <div className={styles.container}>
+                <div className={styles.item}>
+                    <div className={styles.header}>
+                        <input type="text" name="title" value={recipe.title} onChange={handleValueChange}/>
+                    </div>
+                    <div className={styles.main}>
+                        <input type="text" name="description" value={recipe.description} onChange={handleValueChange}/>
+                    </div>
+                    <div className={styles.footer}>
+                        <input type="text" name="author" value={recipe.author} onChange={handleValueChange}/>
+                    </div>
+                    <div className={styles.btnBlock}>
+                        <button
+                            className={styles.toggleBtn}
+                            onClick={toggleEdit}>Edit mode</button>
+                        <button
+                            className={styles.toggleBtn}
+                            onClick={handleSubmit}>Submit edit</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     return (
         <div className={styles.container}>
             <div className={styles.item}>
@@ -13,6 +60,11 @@ const Recipe = (props) => {
                 </div>
                 <div className={styles.footer}>
                     {props.author}
+                </div>
+                <div className={styles.btnBlock}>
+                    <button
+                        className={styles.toggleBtn}
+                        onClick={toggleEdit}>Edit mode</button>
                 </div>
             </div>
         </div>
