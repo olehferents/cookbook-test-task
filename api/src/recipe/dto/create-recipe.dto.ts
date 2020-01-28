2750ae7bd8ddde119bdcbaf9c4b8bf00d0858b0b
Oleg Ferenc <olegferenc1@gmail.com>
2020-01-28 18:32:09 +0200

    Add constuctor, Expose and Exclude decorators

--- api/src/recipe/dto/create-recipe.dto.ts
+++ api/src/recipe/dto/create-recipe.dto.ts
@@ -1,15 +1,26 @@
 import {IsNotEmpty, IsString} from 'class-validator';
+import {Exclude, Expose} from 'class-transformer';
 
+@Exclude()
 export class CreateRecipeDto {
+    constructor(title: string, description: string, author: string) {
+        this.title = title;
+        this.description = description;
+        this.author = author;
+    }
+
     @IsNotEmpty()
     @IsString()
+    @Expose()
     readonly title: string;
 
     @IsNotEmpty()
     @IsString()
+    @Expose()
     readonly description: string;
 
     @IsNotEmpty()
     @IsString()
+    @Expose()
     readonly author: string;
 }
